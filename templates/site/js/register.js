5c25d6995e471ed0d8f4646373d0a89480de314d
Omer Akram <om26er@gmail.com>
2017-08-12 15:01:19 +0500

    Add validation to the registration form

--- templates/site/js/register.js
+++ templates/site/js/register.js
@@ -50,9 +50,6 @@
             },
             "company_details.registration_number": {
                 validators: {
-                     stringLength: {
-                        min: 8
-                    },
                     notEmpty: {
                         message: 'Please supply your company registration number'
                     }
@@ -60,22 +57,4 @@
             }
             }
         })
-        .on('success.form.bv', function(e) {
-            $('#success_message').slideDown({ opacity: "show" }, "slow") // Do something ...
-                $('#contact_form').data('bootstrapValidator').resetForm();
-
-            // Prevent form submission
-            e.preventDefault();
-
-            // Get the form instance
-            var $form = $(e.target);
-
-            // Get the BootstrapValidator instance
-            var bv = $form.data('bootstrapValidator');
-
-            // Use Ajax to submit form data
-            $.post($form.attr('action'), $form.serialize(), function(result) {
-                console.log(result);
-            }, 'json');
-        });
 });
